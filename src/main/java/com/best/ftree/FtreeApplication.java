b63f7e773ff96e1ba4b4d2e0773bd5f8e8acab5c
Vladislav Manzhula <jay.k0lad@gmail.com>
2020-06-08 18:20:25 +0300

    Initial commit

---
+++ src/main/java/com/best/ftree/FtreeApplication.java
@@ -0,0 +1,13 @@
+package com.best.ftree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/best/ftree/FtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.best.ftree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
